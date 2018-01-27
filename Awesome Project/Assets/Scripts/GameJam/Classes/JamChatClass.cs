b24cac7368c96593453440b26df2bd7888b8ecfc
Gurido <guriraccoon@gmail.com>
2018-01-27 14:10:43 +0100

    1

---
+++ Awesome Project/Assets/Scripts/GameJam/Classes/JamChatClass.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JamMessage{
+	public int ID;
+	public int User;
+	public string Content;
+
+	public JamMessage(int id, int user, string content){
+		ID = id;
+		User = user;
+		Content = content;
+
+	}
+
+
+}
+
+
+
+public class JamChat{
+	
+	public int ID;
+	public int User;
+	public List<JamMessage> Message;
+
+	public JamChat(int id, int user){
+
+		ID = id;
+		User = user;
+		Message = new List<JamMessage> ();
+
+	}
+
+}
+
+
+
